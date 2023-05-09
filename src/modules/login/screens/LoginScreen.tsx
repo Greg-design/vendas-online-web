bc5b5e38516feee9343261cff35142e17dcd760a
Greg-design <gregory_gr@outlook.com>
2023-05-09 14:43:21 -0300

    insert axios and first request

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { ChangeEvent, useState } from "react";
 import Button from "../../../shared/buttons/button/Button";
 import Input from "../../../shared/inputs/input/Input";
@@ -11,20 +12,34 @@ import {
 } from "../styles/loginScreen.styles";
 
 const LoginScreen = () => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleUsername = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleEmail = (e: ChangeEvent<HTMLInputElement>) => {
     //e.preventDefault();
-    setUsername(e.target.value);
+    setEmail(e.target.value);
   };
 
   const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
 
-  const handleLogin = () => {
-    alert(`Usuário: ${username}, senha: ${password} `);
+  const handleLogin = async () => {
+    await axios({
+      method: "post",
+      url: "http://localhost:8080/auth",
+      data: {
+        email: email,
+        password: password,
+      },
+    })
+      .then((res) => {
+        alert(`Fez Login ${res.data.accessToken}`);
+        return res.data;
+      })
+      .catch(() => {
+        alert("Usuário ou senha inválidos");
+      });
   };
 
   return (
@@ -36,7 +51,7 @@ const LoginScreen = () => {
           <TitleLogin level={2} type="secondary">
             LOGIN
           </TitleLogin>
-          <Input title="USUÁRIO" margin="32px 0px 0px" onChange={handleUsername} value={username} />
+          <Input title="USUÁRIO" margin="32px 0px 0px" onChange={handleEmail} value={email} />
           <Input type="password" title="SENHA" margin="32px 0px 0px" onChange={handlePassword} value={password} />
           <Button type="primary" margin="64px 0px 16px 0px" onClick={handleLogin}>
             ENTRAR
