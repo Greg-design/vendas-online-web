588b17ffcfd91c90c29d1a7cc1a2d20cc4518076
Greg-design <gregory_gr@outlook.com>
2023-05-09 16:57:55 -0300

    insert react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
+import type { Router as RemixRouter } from "@remix-run/router";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { RouteObject, RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
+import { loginRoutes } from "./modules/login/routes";
+
+const mainRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <div>Tela Principal</div>,
+    errorElement: <div>Página não encontrada!</div>,
+  },
+];
+
+const router: RemixRouter = createBrowserRouter([...mainRoutes, ...loginRoutes]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
