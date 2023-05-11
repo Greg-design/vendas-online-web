695c998f0442e4d86cfa0fa038481d773edca30a
Greg-design <gregory_gr@outlook.com>
2023-05-11 08:57:05 -0300

    insert notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import LoginScreen from "./modules/login";
+import type { Router as RemixRouter } from "@remix-run/router";
+import { RouteObject, RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css";
+import { loginRoutes } from "./modules/login/routes";
+import { useNotification } from "./shared/hooks/useNotification";
+
+const mainRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <div>Tela Principal</div>,
+    errorElement: <div>Página não encontrada!</div>,
+  },
+];
+
+const router: RemixRouter = createBrowserRouter([...mainRoutes, ...loginRoutes]);
 
 function App() {
-  return <LoginScreen />;
+  const { contextHolder } = useNotification();
+  return (
+    <>
+      {contextHolder}
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
--- src/shared/hooks/useRequests.ts
+++ src/shared/hooks/useRequests.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { useState } from "react";
+import { useGlobalContext } from "./useGlobalContext";
 
 export const useRequests = () => {
   const [loading, setLoading] = useState(false);
+  const { setNotification } = useGlobalContext();
 
   const getRequest = async (url: string) => {
     setLoading(true);
@@ -27,11 +29,11 @@ export const useRequests = () => {
       data: body,
     })
       .then((res) => {
-        alert(`Fez Login`);
+        setNotification("Entrando...", "success");
         return res.data;
       })
       .catch(() => {
-        alert("Usuário ou senha inválidos");
+        setNotification("Senha inválida", "error");
       });
 
     setLoading(false);
